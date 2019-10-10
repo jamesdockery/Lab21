295b417d1830f8a7e1330567e6c56006ea27bf59
James <jamesdockery011@gmail.com>
2019-10-10 10:26:42 -0400

    Added User Validation

--- CoffeeShop/Models/User.cs
+++ CoffeeShop/Models/User.cs
@@ -10,23 +10,47 @@ namespace CoffeeShop.Models
     public class User
     {
         [Key]
-        [Required(ErrorMessage ="User ID is required")]
         public int UserID { get; set; }
+
+
+
+
         [Required(ErrorMessage ="First Name is required")]
         public string FirstName { get; set; }
+
+
+
+
         [Required(ErrorMessage ="Last Name is required")]
         public string LastName { get; set; }
+
+
+
+        
         [Required(ErrorMessage ="User Name is required")]
+        [StringLength(10, MinimumLength = 3)]
+
         public string UserName { get; set; }
+
+
+
         [Required(ErrorMessage ="Email is required")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
-            [Required(ErrorMessage ="Password is required")]
+
+
+     
+        [Required(ErrorMessage ="Password is required")]
             [DataType(DataType.Password)]
+        [MinLength(3)]
+        [MaxLength(10)]
         public string Password { get; set; }
+
+
         [Required(ErrorMessage ="Confirm Password is required")]
         [Compare("Password", ErrorMessage ="Passwords must match")]
         [DataType(DataType.Password)]
+
         public string ConfirmPassword { get; set; }
 
        
